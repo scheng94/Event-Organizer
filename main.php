ae161d36af4af588f1eb3c8e9e0e62ce4e5e9cda
scheng94 <samtcheng94@gmail.com>
2017-06-25 19:42:35 -0400

    Created Main Page and Logins

--- main.php
+++ main.php
@@ -1,2 +1,21 @@
 <?php
+    require("support.php");
+    
+    if(isset($_POST["managerLogin"])){
+        header("Location: managerLogin.php");
+    } elseif(isset($_POST["participantLogin"])){
+        header("Location: participantLogin.php");
+    }
+
+    $body = <<<EOBODY
+    <h2>Plan and Join Events Near You!</h2>
+    <form action="main.php" method="post">
+        <input type="button" name="managerLogin" value="Event Manager Login"/><br><br>
+        <input type="button" name="participantLogin" value="Participant Login"/>
+    </form>
+
+EOBODY;
+
+    $page = generatePage($body,"Event Organizer");
+    echo $page;
 ?>
